f19a7bb64ae443f2e56999fed91a432c1ff64ce4
Egor Nikolaev <egornikolaev97@yandex.ru>
2022-02-20 04:04:02 +0500

    feat: add sumbit form

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,7 @@
 let editProfile = document.querySelector('.profile__edit-btn');
-let closePopup = document.querySelector('.popup__close');
 let popup = document.querySelector('.popup');
+let closePopup = document.querySelector('.popup__close');
+let sumbit = document.querySelector('form__submit')
 
 editProfile.addEventListener('click', function() {
     popup.classList.add('popup_opened');
@@ -8,3 +9,30 @@ editProfile.addEventListener('click', function() {
 closePopup.addEventListener('click', function() {
     popup.classList.remove('popup_opened');
 });
+
+
+
+// Редактирование имени и информации о себе //
+
+let formElement = document.querySelector('.form');
+let nameInput = formElement.querySelector('.form__input_name');
+let aboutInput = formElement.querySelector('.form__input_about');
+let submit = formElement.querySelector('.form__submit');
+
+
+function formSubmitHandler (evt) {
+    evt.preventDefault();
+
+let profileName = document.querySelector('.profile__name');
+let profileAbout = document.querySelector('.profile__about');
+
+profileName.textContent = nameInput.value;
+profileAbout.textContent = aboutInput.value;
+}
+
+submit.addEventListener('click', function() {
+    popup.classList.remove('popup_opened');
+});
+
+
+formElement.addEventListener('submit', formSubmitHandler);
