b7c7ee611572fbd37aa076f5dc1a3546808491a5
Egor Nikolaev <egornikolaev97@yandex.ru>
2022-04-22 21:12:20 +0500

    delete initialCards from index.js

--- scripts/index.js
+++ scripts/index.js
@@ -25,35 +25,6 @@ const profileAbout = document.querySelector('.profile__about');
 //input elements for add cards popup
 const cardNameInput = popupAdd.querySelector('.form__input_type_title');
 const cardLinkInput = popupAdd.querySelector('.form__input_type_link');
-//for show image
-// const imagePopup = popupShow.querySelector('.popup__image');
-// const imageDescription = popupShow.querySelector('.popup__description');
-
-// const initialCards = [{
-//     name: 'Архыз',
-//     link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
-//   },
-//   {
-//     name: 'Челябинская область',
-//     link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
-//   },
-//   {
-//     name: 'Иваново',
-//     link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
-//   },
-//   {
-//     name: 'Камчатка',
-//     link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
-//   },
-//   {
-//     name: 'Холмогорский район',
-//     link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
-//   },
-//   {
-//     name: 'Байкал',
-//     link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
-//   }
-// ];
 
 const config = {
   formSelector: '.form',
